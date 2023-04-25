b57af6407d65ead36e9c4e864aaf00f887177f9c
Abhinav <abhinav@AVs-Mac.local>
2023-04-25 21:07:22 +0530

    [25 Apr 2023] JS intro, JS variable.

---
+++ JS_Basics/script.js
@@ -0,0 +1,37 @@
+console.log("running JS from script file");
+// case -sensitive
+// variables
+// --> way to store data so that we can use it anywhere imn our program
+
+// declaring a variable called 'a' and assigning a value 23 to it
+
+var a = 23;
+// var--> keyword
+// a --> variable name
+// = --> assignment operator
+// 23 --> value
+
+// console.log(a+23);
+
+console.log(a);
+// it means Javascript is executed line by line one after the other --> syncronous in nature
+a = 50;
+console.log(a);
+
+var username= 'Abhinav';
+console.log(username);
+
+// javascript is a dynamically typed language
+// int a
+
+username = 100
+console.log(username);
+
+// Rules of defining a variable
+// 1. can not be already defined javascript keyword ex: var var = 20
+// 2. letters or characters and numbers and _, $ var abhinav_ins456 = 'instructor', 
+// 3. cant start with number
+
+var _asb = ''
+var _ASB;
+_ASB = 30
