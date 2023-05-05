99698dfccdf8408a8e094d2996eb43bcaa755f8c
Abhinav <abhinav@AVs-Mac.local>
2023-05-05 23:45:05 +0530

    [5 may 2023] lexical scope, let vs const vs var scope, array basic

---
+++ JS_Basics/Arrays.js
@@ -0,0 +1,64 @@
+const arr = [1,4,5,65,43];
+/*
+console.log(arr);
+console.log(arr[3]);
+
+console.log(arr.at(3));
+// length
+console.log(arr.length);
+console.log(arr[0]);
+// const lastIndex = arr.length-1
+console.log(arr[arr.length-1]);
+
+// operations on arrays
+// for inserting an el
+
+// push --> changes the original array
+// insert from end
+console.log(arr);
+arr.push(58);
+let res = arr.push(56, 78);
+console.log('res', res); //updated length 
+console.log(arr);
+
+
+
+// pop --> changes the original array
+console.log(arr);
+let res = arr.pop();
+console.log('res', res);
+console.log(arr);
+
+// unshift -> changes the original array
+// insert from top
+console.log(arr);
+let res = arr.unshift(23, 89);
+console.log('res', res); //updated length
+console.log(arr);
+
+
+
+// unshift -> changes the original array
+// remove from top
+console.log(arr);
+let res = arr.shift();
+console.log('res', res);
+console.log(arr);
+
+// update the values at diff indexes by assigning the value
+arr[0] = 33
+console.log(arr);
+*/
+
+
+
+// function(){
+
+// }
+
+
+
+
+
+
+
--- JS_Basics/scoping.js
+++ JS_Basics/scoping.js
@@ -1,3 +1,4 @@
+/*
 let a = 155;
 console.log('in global', a);
 
@@ -16,12 +17,111 @@ function calculate(){
             console.log(g);
             console.log(x);
         }
-        twoMultiplier()
+        twoMultiplier();
     }
-    multiplication()
+    multiplication();
 }
 calculate();
 
 // lexical scoping in function
 // scope chaining
 
+
+
+function GrandParent(){
+    let gp = 300;
+    console.log(gp);
+    function parent(){
+        let p = 50;
+        console.log('p is ', p);
+        console.log('gp is', gp);
+    }
+    parent();
+    console.log(p);
+    console.log(a);
+}
+
+GrandParent();
+
+// console.log(r);
+
+// let and const has block scope 
+
+
+let a = 155;
+console.log('in global', a);
+
+if(a === 155){
+    let var2 = 'somename'
+    let var3 = 'var3'
+    console.log(var2);
+}
+
+console.log(var1);
+console.log(var2); //not accessible
+
+
+// var has functional scope
+// console.log(a); //undefined
+// console.log(c); //
+
+var a = 40
+let c = 50
+
+if(a === 155){
+    let var3 = 'var3'
+    var b = 'var4'
+    console.log(var3);
+    console.log(b);
+}
+
+console.log(b);
+*/
+
+let a = 20;
+function test(){
+    let a = 30;
+    if(a===20){
+        var b = 20
+    }
+    console.log(a);//30
+    console.log(b);//undefined
+}
+test();
+console.log(a);//20
+// console.log(b); //error
+
+
+
+
+
+/*
+// block of code examples
+if(a === 20){
+    console.log('inside if');
+    let a = 30
+    let b = 50
+    console.log(a*b);
+} else{
+    console.log('inside else');
+    let x = 34
+    let y = 50
+    console.log(x+y);
+}
+
+
+for(let i = 0; i< 8; i++){
+
+}
+
+
+
+
+let b = 20;
+
+let obj = {
+    name: 'Abhinav'
+}
+
+let arr = []
+*/
