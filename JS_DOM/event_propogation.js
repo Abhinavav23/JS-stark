d008905555d16ce6fe0d31a647a769b2ed5a9c46
Abhinav <abhinav@AVs-Mac.local>
2023-05-18 23:24:43 +0530

    [18 may 2023] event propogation, bubbling, capturing, stoppropogation

--- JS_DOM/event_propogation.js
+++ JS_DOM/event_propogation.js
@@ -12,14 +12,60 @@ const buttonEl = document.querySelector('button');
 
 // vice versa of bubbling is called capturing
 
+// arguments
+// 1. event name
+// 2. handler callback function
+// 3. useCapture --> true --> capturing || false --> bubbling
+// by default behaviour is bubbling or usecapture value is false
+
 mainEl.addEventListener('click', () => {
     console.log('clicked n main element');
-});
+}); // bubbling
 
-sectionEl.addEventListener('click', () => {
+sectionEl.addEventListener('click', (event) => {
+    console.log(event.cancelable);
+    // console.log(event.target);
+    // event.stopPropagation();
+    // this actually cancels the execution of of any default behaviour for that event
+    // cacelable property decides if we would be able to cancel the default behaviour
+    // event.preventDefault();
     console.log('clicked n section element');
-})
+}, true) //capturing
 
-buttonEl.addEventListener('click', () => {
+
+function buttonHandler(event){
+    // event.stopPropagation();
+    // event.stopImmediatePropagation()
     console.log('clicked n button element');
-})
+} //bubbling
+
+// buttonEl.addEventListener('click', (event) => {
+//     console.log(event);
+//     console.log('clicked n button element');
+// })
+function buttonHandler2(event){
+    // event.stopPropagation();
+   
+    console.log('clicked n button element again');
+} //bubbling
+
+buttonEl.addEventListener('click', buttonHandler)
+buttonEl.addEventListener('click', buttonHandler2)
+
+// function addEventListener(eventName, callbackFn, usecapture=false){
+
+// }
+
+// capturing -->
+// main ---> capturing --> 1st step --> while going
+// section --> bubbling --> coming back --
+// button --> capturing --> 3 step --> while going --------Destination means target element
+
+// bubbling
+// button ---> 
+// section --> lets execute ---> 
+// main --> 
+
+// main
+// button
+// section
